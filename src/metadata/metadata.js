9a576fe319fb3b3cc326c81d7c580bf9b81ef90a
Troy P. Bernier <troybernier@gmail.com>
2018-09-16 18:09:44 -0400

    resumeupdated

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -10,91 +10,81 @@ module.exports = {
       title: 'Azimuth GIS',
       written: '2018 -Present',
       badges: ['GIS', 'natural disaster', 'petroleum', 'environment', 'mining'],
-      contents: `
-Perform work distribution and management of team members
-Follow development and implement data infrastructure, including the execution of test plans, and system documentation
-Effectively manage multiple projects and clients simultaneously while continuing to provide high-quality delivery and support
-Develops lasting relationships with client team members
-Rapid development of GIS and GeoPlatform applications and services for critical or emergency events
-`
+      contents: '
+Perform work distribution and management of team members  Follow development and implement data infrastructure, including the execution of test plans, and system documentation  Effectively manage multiple projects and clients simultaneously while continuing to provide high-quality delivery and support  Develops lasting relationships with client team members  Rapid development of GIS and GeoPlatform applications and services for critical or emergency events
+'
     },
     {
       title: 'Ph.D. Candidate in Geoscience; Florida Atlantic University, 777 Glades Road, Boca Raton, FL ',
       written: '2012 -Present',
       badges: ['imagery', 'geology', 'hydrology', 'isotopes', 'geospatial']
-      contents: `
-Built spatial databases containing USGS lithological and DBHydro water level data.
-Produced two Broward County subsurface models with Groundwater Vistas and results were calibrated with PEST.
-Used AVIRIS Hyperspectral data to build an ERDAS geological classification schedules for the interpretation of surface minerals.
-`
+      contents: '
+Built spatial databases containing USGS lithological and DBHydro water level data.  Produced two Broward County subsurface models with Groundwater Vistas and results were calibrated with PEST.  Used AVIRIS Hyperspectral data to build an ERDAS geological classification schedules for the interpretation of surface minerals.
+'
     },
     {
       title: 'Consultant; H2OResource, Inc., 9715 W. Broward Blvd, Plantation, Florida ',
       written: '2010 -Present',
       badges: ['evapotranspiration', 'licor', 'picarro', 'florida statutes'],
-      contents: `
-Subject matter expert on Florida Statutes Title XXVIII (Natural Resources; Conservation, Reclamation, and Use), Title XI (Water and Sewers), and Title XXXII (Regulation of Professions and Occupations). Including practice in chapters 627, 403, 492, 373, 376, 377, 378, 380, and 153.
-Subcontractor to the USGS and Archbold Biological Station for the construction of flux towers and using Eddypro to produce evapotranspiration reports.
-`
+      contents: '
+Subject matter expert on Florida Statutes Title XXVIII (Natural Resources; Conservation, Reclamation, and Use), Title XI (Water and Sewers), and Title XXXII (Regulation of Professions and Occupations). Including practice in chapters 627, 403, 492, 373, 376, 377, 378, 380, and 153.  Subcontractor to the USGS and Archbold Biological Station for the construction of flux towers and using Eddypro to produce evapotranspiration reports.
+'
     },
     {
 	      title: 'Consultant; Hydrocarbon Imaging Services, Inc.   Senegal, and Guinea-Bissau',
 	      written: '2014',
 	      badges: ['geochemistry', 'geology', 'radiometrics', 'geospatial']
-	      contents: `
-	Team leader for the administration, testing and use of an advanced geophysical radiometric exploration technology application in West Africa for interpreting surficial geochemistry.
-	Integrated geophysical imagery data on Google Earth to produce informative scientific maps which visualized the surface geology in the multi-block survey.
-	`
+	      contents: '
+	Team leader for the administration, testing and use of an advanced geophysical radiometric exploration technology application in West Africa for interpreting surficial geochemistry.  	Integrated geophysical imagery data on Google Earth to produce informative scientific maps which visualized the surface geology in the multi-block survey.
+	'
     },
     {
 	      title: 'Field Seismic Analyst; Western Geophysical, GOM, Europe, and West Africa',
 	      written: '1997 -1999',
 	      badges: ['geophysics', 'geology', '4c']
-	      contents: `
-	Required to build and maintain comprehensive processing workflows using the Omega geophysical software platform.
-	Verified quality control methods with 2D and 3D visual interpretations.
-	`
+	      contents: '
+	Required to build and maintain comprehensive processing workflows using the Omega geophysical software platform.  	Verified quality control methods with 2D and 3D visual interpretations.
+	'
     },
        {
 		      title: 'Adjunct Faculty; Miami Dade College,  Miami, FL',
 		      written: '1999 – 2012',
 		      badges: ['natural science', 'geology', 'esri arc gis', 'database design']
-		      contents: `
+		      contents: '
 		Classroom instructor in Natural Science, Geology, ESRI ARC GIS, and Database Design
-		`
+		'
     },
        {
 		      title: 'Hydrologist; US Geological Survey, 9100 NW 36th St, Miami, FL',
 		      written: '1996 -1997',
 		      badges: ['hydrology', 'water quality', 'gps', 'saltwater-intrusion', 'geospatial']
-		      contents: `
-		Produced monthly water-level and saltwater intrusion reports for stakeholders.
-		Introduced GPS technology to the South Florida district with Ashtech research grade hardware and software.
-		`
+		      contents: '
+		Produced monthly water-level and saltwater intrusion reports for stakeholders.    Introduced GPS technology to the South Florida district with Ashtech research grade hardware and software.
+		'
     },
        {
 		      title: 'Hydrologic Technician; US Geological Survey, 3916 Sunset Ridge Rd, Raleigh, NC',
 		      written: '1992 -1995',
 		      badges: ['water quality', 'hydrogeology', 'resistivity', 'gps', 'geospatial', 'electromagnetic', 'geophysics']
-		      contents: `
-		Produced a hydrogeologic framework of the surficial aquifer and Yorktown formation for the NAWQA Pete Mitchell Swamp intense study area using state well-logs, borehole geophysical data and acquired electromagnetic resistivity data.
-		`
+		      contents: '
+		Produced a site concepualized model including the surficial aquifer and lower Yorktown formation for the NAWQA Pete Mitchell Swamp intense study area using state well-logs, borehole geophysical data and acquired electromagnetic resistivity data.
+		'
     },
   ],
   experience: [
     {
       header: 'Global Trekking',
       skills: ['switzerland', 'uk', 'west africa'],
-      contents: `
+      contents: '
 
-`
+'
     },
     {
       header: 'Motion Photography',
       skills: ['canon', 'drone'],
-      contents: `
+      contents: '
 
-`
+'
     }
   ]
 };
