213294a954e83e0e05af77c35f8bae82531f2613
Troy P. Bernier <troybernier@gmail.com>
2018-09-16 17:19:50 -0400

    resumeupdated

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,49 +1,99 @@
 module.exports = {
   updated: new Date(),
   facts: {
-    'Residence': 'London',
-    'Homepage': '<a href="https://en.wikipedia.org/wiki/William_Shakespeare">https://en.wikipedia.org/wiki/William_Shakespeare</a>',
-    'Email': '<a href="mailto:wil64@yahoo.com">wil64@yahoo.com</a>'
+    'Residence': 'United States',
+    'Homepage': '<a href="https://troybernier.com">https://troybernier.com</a>',
+    'Email': '<a href="mailto:troybernier@gmail.com">troybernier@gmail.com</a>'
   },
   plays: [
     {
-      title: 'Hamlet',
-      written: 'Likely early 17th century',
-      badges: ['tragedy', 'philosophy', 'death'],
+      title: 'Azimuth GIS',
+      written: '2018 -Present',
+      badges: ['GIS', 'natural disaster', 'petroleum', 'environment', 'mining'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+Perform work distribution and management of team members
+Follow development and implement data infrastructure, including the execution of test plans, and system documentation
+Effectively manage multiple projects and clients simultaneously while continuing to provide high-quality delivery and support
+Develops lasting relationships with client team members
+Rapid development of GIS and GeoPlatform applications and services for critical or emergency events
 `
     },
     {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
+      title: 'Ph.D. Candidate in Geoscience; Florida Atlantic University, 777 Glades Road, Boca Raton, FL ',
+      written: '2012 -Present',
+      badges: ['imagery', 'geology', 'hydrology', 'isotopes', 'geospatial']
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+Built spatial databases containing USGS lithological and DBHydro water level data.
+Produced two Broward County subsurface models with Groundwater Vistas and results were calibrated with PEST.
+Used AVIRIS Hyperspectral data to build an ERDAS geological classification schedules for the interpretation of surface minerals.
 `
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
+      title: 'Consultant; H2OResource, Inc., 9715 W. Broward Blvd, Plantation, Florida ',
+      written: '2010 -Present',
+      badges: ['evapotranspiration', 'licor', 'picarro', 'florida statutes'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+Subject matter expert on Florida Statutes Title XXVIII (Natural Resources; Conservation, Reclamation, and Use), Title XI (Water and Sewers), and Title XXXII (Regulation of Professions and Occupations). Including practice in chapters 627, 403, 492, 373, 376, 377, 378, 380, and 153.
+Subcontractor to the USGS and Archbold Biological Station for the construction of flux towers and using Eddypro to produce evapotranspiration reports.
 `
-    }
+    },
+    {
+	      title: 'Consultant; Hydrocarbon Imaging Services, Inc.   Senegal, and Guinea-Bissau',
+	      written: '2014',
+	      badges: ['geochemistry', 'geology', 'radiometrics', 'geospatial']
+	      contents: `
+	Team leader for the administration, testing and use of an advanced geophysical radiometric exploration technology application in West Africa for interpreting surficial geochemistry.
+	Integrated geophysical imagery data on Google Earth to produce informative scientific maps which visualized the surface geology in the multi-block survey.
+	`
+    },
+    {
+	      title: 'Field Seismic Analyst; Western Geophysical, GOM, Europe, and West Africa',
+	      written: '1997 -1999',
+	      badges: ['geophysics', 'geology', '4c']
+	      contents: `
+	Required to build and maintain comprehensive processing workflows using the Omega geophysical software platform.
+	Verified quality control methods with 2D and 3D visual interpretations.
+	`
+    },
+       {
+		      title: 'Adjunct Faculty; Miami Dade College,  Miami, FL',
+		      written: '1999 � 2012',
+		      badges: ['natural science', 'geology', 'esri arc gis', 'database design']
+		      contents: `
+		Classroom instructor in Natural Science, Geology, ESRI ARC GIS, and Database Design
+		`
+    },
+       {
+		      title: 'Hydrologist; US Geological Survey, 9100 NW 36th St, Miami, FL',
+		      written: '1996 -1997',
+		      badges: ['hydrology', 'water quality', 'gps', 'saltwater-intrusion', 'geospatial']
+		      contents: `
+		Produced monthly water-level and saltwater intrusion reports for stakeholders.
+		Introduced GPS technology to the South Florida district with Ashtech research grade hardware and software.
+		`
+    },
+       {
+		      title: 'Hydrologic Technician; US Geological Survey, 3916 Sunset Ridge Rd, Raleigh, NC',
+		      written: '1992 -1995',
+		      badges: ['water quality', 'hydrogeology', 'resistivity', 'gps', 'geospatial', 'electromagnetic', 'geophysics']
+		      contents: `
+		Produced a hydrogeologic framework of the surficial aquifer and Yorktown formation for the NAWQA Pete Mitchell Swamp intense study area using state well-logs, borehole geophysical data and acquired electromagnetic resistivity data.
+		`
+    },
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      header: 'Global Trekking',
+      skills: ['switzerland', 'uk', 'west africa'],
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
+
 `
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
+      header: 'Motion Photography',
+      skills: ['canon', 'drone'],
       contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
+
 `
     }
   ]
